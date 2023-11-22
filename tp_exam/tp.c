1bece78a9b4798f3500c70e677e8d23520f09708
Chaman-veteran <maillen@bde.enseeiht.fr>
2023-11-22 15:20:41 +0100

    remove elf

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -2,6 +2,12 @@
 #include <debug.h>
 #include "interruptions/interruption.h"
 
+// TODO: ajouter la section dans le linker
+/*
+void __attribute__((section(".user"))) user1() { while (1) {} }
+void __attribute__((section(".user"))) user2() { while (1) {} }
+*/
+
 void tp() {
 	// TODO
 }
